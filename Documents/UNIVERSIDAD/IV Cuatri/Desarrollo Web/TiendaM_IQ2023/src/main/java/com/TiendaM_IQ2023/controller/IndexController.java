79d7618fc0c04946c3c311b084b014df9fb4d960
joss896 <josselinejimenez8@gmail.com>
2023-02-15 17:56:59 -0600

    Finalización de la clase 04

--- Documents/UNIVERSIDAD/IV Cuatri/Desarrollo Web/TiendaM_IQ2023/src/main/java/com/TiendaM_IQ2023/controller/IndexController.java
+++ Documents/UNIVERSIDAD/IV Cuatri/Desarrollo Web/TiendaM_IQ2023/src/main/java/com/TiendaM_IQ2023/controller/IndexController.java
@@ -2,9 +2,11 @@
 package com.TiendaM_IQ2023.controller;
 
 import com.TiendaM_IQ2023.domain.Cliente;
+import com.TiendaM_IQ2023.dao.ClienteDao;
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +15,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     
+    @Autowired
+    ClienteDao clienteDao;
+    
     @GetMapping("/")
     public String inicio(Model model){
-        log.info("Ahora desde MVC");
-        model.addAttribute("Mensaje", "Hola desde el controlador");
-        Cliente cliente = new Cliente("Josseline", "Jiménez Carmona", "josselinejimenez8@gmail.com", "85889822");
-        Cliente cliente2 = new Cliente("Juan", "Mora Quesada", "jumora@gmail.com", "89652836");
-        Cliente cliente3 = new Cliente("Mauricio", "Sánchez Aragón", "wilim11@hotmail.com", "87533773");
+      //log.info("Ahora desde MVC");
+        //model.addAttribute("Mensaje", "Hola desde el controlador");
+        //Cliente cliente = new Cliente("Josseline", "Jiménez Carmona", "josselinejimenez8@gmail.com", "85889822");
+        //Cliente cliente2 = new Cliente("Juan", "Mora Quesada", "jumora@gmail.com", "89652836");
+        //Cliente cliente3 = new Cliente("Mauricio", "Sánchez Aragón", "wilim11@hotmail.com", "87533773");
         
-        model.addAttribute("objetoCliente", cliente);      
-        List<Cliente> clientes = Arrays.asList(cliente, cliente2, cliente3);
+        //model.addAttribute("objetoCliente", cliente);      
+        //List<Cliente> clientes = Arrays.asList(cliente, cliente2, cliente3);
+        
+        var clientes = clienteDao.findAll();
         model.addAttribute("Clientes", clientes);
         return "index";
     }
